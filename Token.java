a8df24c533de44100bb0b4727a1e5f2647a3a34c
webberc <webberc@augsburg.edu>
2020-01-25 03:11:56 -0600

    added equals method, setter methods

--- Token.java
+++ Token.java
@@ -7,6 +7,42 @@
 	String lexeme; 
 	TokenType type; 
 	
+	/**
+	 * Constructor method - takes in lexeme and type 
+	 * @param lexeme The lexeme String
+	 * @param type The Token Type 
+	 */
+	public Token(String lexeme, TokenType type) {
+		this.setLexeme(lexeme); 
+		this.setTT(type);
+	}
+	
+	/** 
+	 * Setter method - defines this token's lexeme. 
+	 * @param newLex The lexeme String
+	 */
+	public void setLexeme(String newLex) {
+		this.lexeme = newLex;
+	}
+	
+	/** 
+	 * Setter method - defines this token's Token Type. 
+	 * @param tt The Token Type
+	 */
+	public void setTT(TokenType tt) {
+		this.type = tt;
+	}
+	
+	@Override 
+	public boolean equals(Object o) {
+		if (o == this) { return true; }
+		
+		if (!(o instanceof Token)) { return false; }
+		
+		Token t = (Token) o; 
+		return (lexeme.equals(t.lexeme)) && (type.equals(t.type); 
+	}
+	
 	@Override
 	public String toString() {
 		return lexeme + " : " + type;
