aa907701ce16465293272c6904576a8b39640baf
webberc <webberc@augsburg.edu>
2020-01-25 23:45:00 -0600

    this exception might be helpful?

---
+++ Test/Scanner/SadTestException.java
@@ -0,0 +1,12 @@
+/**
+ * Exception thrown in case of a Sad Test
+ * CSC 451
+ * @author Chase Webber
+ */
+public class SadTestException extends Exception {
+
+	public SadTestException(String message) {
+		super(message);
+	}
+
+}
