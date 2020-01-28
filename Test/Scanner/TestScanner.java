f799a503235f65a8a43e117cbaba562b2e34fafd
webberc <webberc@augsburg.edu>
2020-01-28 14:07:23 -0600

    updated tests, two failed.

--- Test/Scanner/TestScanner.java
+++ Test/Scanner/TestScanner.java
@@ -15,7 +15,7 @@ public class TestScanner {
     
     public TestScanner() {
     }
-    
+	
     @BeforeClass
     public static void setUpClass() {
     }
@@ -32,9 +32,11 @@ public class TestScanner {
     public void tearDown() {
     }
 
+	
     /**
      * Test of nextToken method, of class Scanner.
      */
+    /*
     @Test
     public void testNextToken() throws Exception {
         String test = "14+3";
@@ -55,63 +57,57 @@ public class TestScanner {
         actual = s.nextToken();
         assertNull(actual);        
     }
+    */ 
 	
-	
-	//PREVIOUS FUNCITONS... 
-    
+    /***** TEST NUMBERS *****/
 
-	/***** TEST NUMBERS *****/
-
-	/* Test integer */
-	@Test
-	void testNumber1() throws IOException {
-		String test = "3";
-        Scanner s = new Scanner(new StringReader(test));
-
-		actual = s.nextToken();
-        expected = new Token("3", TokenType.NUMBER);
-        assertEquals(expected, actual);
-        
-        actual = s.nextToken();
-        assertNull(actual);        
-    
-	}
-	
-	/* Test floating point */
-	@Test
-	void testNumber2() throws IOException {
-		String test = "3.14";
-        Scanner s = new Scanner(new StringReader(test));
-
-		actual = s.nextToken();
-        expected = new Token("3.14", TokenType.NUMBER);
-        assertEquals(expected, actual);
-        
-        actual = s.nextToken();
-        assertNull(actual);        
-    
-	}
-	
-	/* Test scientific notation */
-	@Test
-	void testNumber3() throws IOException {
-		String test = "3.14159E7";
-        Scanner s = new Scanner(new StringReader(test));
-
-		actual = s.nextToken();
-        expected = new Token("12", TokenType.NUMBER);
-        assertEquals(expected, actual);
-        
-        actual = s.nextToken();
-        assertNull(actual);        
-    
-	}
-	
-	/***** HAPPY TESTS *****/
-
-	/* Tests a basic math expression */
+    /* Test integer */
     @Test
-    void testNextTokenHappy1() throws IOException {
+    public void testNumber1() throws Exception {
+        String test = "3";
+        Scanner s = new Scanner(new StringReader(test));
+
+        Token actual = s.nextToken();
+        Token expected = new Token("3", TokenType.NUMBER);
+        assertEquals(expected, actual);
+
+        actual = s.nextToken();
+        assertNull(actual);        
+    }
+
+    /* Test floating point */
+    @Test
+    public void testNumber2() throws Exception {
+        String test = "3.14";
+        Scanner s = new Scanner(new StringReader(test));
+
+        Token actual = s.nextToken();
+        Token expected = new Token("3.14", TokenType.NUMBER);
+        assertEquals(expected, actual);
+
+        actual = s.nextToken();
+        assertNull(actual);        
+    }
+
+    /* Test scientific notation */
+    @Test
+    public void testNumber3() throws Exception {
+        String test = "3.14159E7";
+        Scanner s = new Scanner(new StringReader(test));
+
+        Token actual = s.nextToken();
+        Token expected = new Token("12", TokenType.NUMBER);
+        assertEquals(expected, actual);
+
+        actual = s.nextToken();
+        assertNull(actual);        
+    }
+
+    /***** HAPPY TESTS *****/
+
+    /* Tests a basic math expression */
+    @Test
+    public void testNextTokenHappy1() throws Exception {
         String test = "14+3";
         Scanner s = new Scanner(new StringReader(test));
         
@@ -131,25 +127,27 @@ public class TestScanner {
         assertNull(actual);        
     }
 	
-	/***** SAD TESTS *****/
-	
-	/* Tests what happens when an illegal symbol is found */
-	@Test
-	void testNextTokenSad1() throws IOException {
-		String test "#"
-		Scanner s = new Scanner(new StringReader(test));
-		
-		Token actual = null; 
-		
-		try {
-			actual = s.nextToken();
-			fail("Oh no! The Sad Test didn't fail!!");
-		} catch ( 
-		
+    /***** SAD TESTS *****/
+
+    /* Tests what happens when an illegal symbol is found */
+    /*
+    @Test
+    public void testNextTokenSad1() throws IOException {
+        String test "#"
+        Scanner s = new Scanner(new StringReader(test));
+
+        Token actual = null; 
+
+        try {
+            actual = s.nextToken();
+            fail("Oh no! The Sad Test didn't fail!!");
+        } catch ( 
+
 		
 		
 		
 	
 	}
+	*/
 
 }
