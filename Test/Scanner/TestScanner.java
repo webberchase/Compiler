0be141a48fc46f51a561bc6c52a6d99072ad024a
webberc <webberc@augsburg.edu>
2020-01-28 09:51:05 -0600

    Fixed JUnit issues. Ready to update tests.

--- Test/Scanner/TestScanner.java
+++ Test/Scanner/TestScanner.java
@@ -1,19 +1,13 @@
-import java.io.IOException;
 import java.io.StringReader;
-import org.junit.Test; 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
  * Unit tests of the Scanner class, to be run with JUnit. 
- * 
- * THESE COMMAND LINE TOOLS DON'T WORK...
- *
- * To compile against the JUnit classes on the command line:
- * javac -cp .;junit-4.13.jar;hamcrest-core-1.3.jar TestScanner.java
- * 
- * To run the tests in this class on the command line:
- * java -cp .;junit-4.13.jar;hamcrest-core-1.3.jar org.junit.runner.JUnitCore TestScanner
- * 
  * CSC 451
  * @author Chase Webber
  */
@@ -21,6 +15,50 @@ public class TestScanner {
     
     public TestScanner() {
     }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of nextToken method, of class Scanner.
+     */
+    @Test
+    public void testNextToken() throws Exception {
+        String test = "14+3";
+        Scanner s = new Scanner(new StringReader(test));
+        
+        Token actual = s.nextToken();
+        Token expected = new Token("14", TokenType.NUMBER);
+        assertEquals(expected, actual);
+        
+        actual = s.nextToken();
+        expected = new Token("+", TokenType.PLUS);
+        assertEquals(expected, actual);
+        
+        actual = s.nextToken();
+        expected = new Token("3", TokenType.NUMBER);
+        assertEquals(expected, actual);
+        
+        actual = s.nextToken();
+        assertNull(actual);        
+    }
+	
+	
+	//PREVIOUS FUNCITONS... 
+    
 
 	/***** TEST NUMBERS *****/
 
