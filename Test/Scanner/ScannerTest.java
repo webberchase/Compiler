5c5e3bf8286e1ae895429999a167a78e592b499d
webberc <webberc@augsburg.edu>
2020-02-16 01:29:11 -0600

    fixed import statements

--- Test/Scanner/ScannerTest.java
+++ Test/Scanner/ScannerTest.java
@@ -11,6 +11,10 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import scanner.Scanner;
+import scanner.Token;
+import scanner.TokenType;
+
 
 /**
  *
