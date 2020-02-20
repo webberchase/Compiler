985ae34e31a8347f91d79db250b85ccc491a2ae9
webberc <webberc@augsburg.edu>
2020-02-20 14:58:57 -0600

    Formatting, comments

--- Test/Scanner/ScannerTest.java
+++ Test/Scanner/ScannerTest.java
@@ -114,61 +114,61 @@ public class ScannerTest {
     /* Tests whitespace */
     @Test
     public void testWhiteSpace() throws Exception {
-            // 10 spaces, 1 tab, 3 more spaces
-            String test = "          \t   ";
-            Scanner s = new Scanner(new StringReader(test));
+		// 10 spaces, 1 tab, 3 more spaces
+		String test = "          \t   ";
+		Scanner s = new Scanner(new StringReader(test));
 
-            for (int i = 0; i < 15; i++) {
-                    Token actual = s.nextToken();
-                    assertEquals(actual, s.getEND());     
-            }
+		for (int i = 0; i < 15; i++) {
+			Token actual = s.nextToken();
+			assertEquals(actual, s.getEND());     
+		}
     }
     
     // FIXME
     /* Test traditional comment */
     @Test
     public void testTraditionalComment() throws Exception {
-            String test = "/* testing traditional comment! */";
+	String test = "/* testing traditional comment! */";
     Scanner s = new Scanner(new StringReader(test));
 
-            for (int i = 0; i < 100; i++) {
-                    Token actual = s.nextToken();
-                    assertEquals(actual, s.getEND());     
-            }
+		for (int i = 0; i < 100; i++) {
+			Token actual = s.nextToken();
+			assertEquals(actual, s.getEND());     
+		}
     }
     
     // FIXME
     /* Test traditional comment */
     @Test
     public void testTraditionalComment2() throws Exception {
-            String test = "/* testing";
-    Scanner s = new Scanner(new StringReader(test));
+		String test = "/* testing";
+		Scanner s = new Scanner(new StringReader(test));
 
-            Token actual = s.nextToken();
-            Token expected = new Token("/", TokenType.DIVIDEDBY);
-    assertEquals(expected, actual);		
+		Token actual = s.nextToken();
+		Token expected = new Token("/", TokenType.DIVIDEDBY);
+		assertEquals(expected, actual);		
 
-            actual = s.nextToken();
-            expected = new Token("*", TokenType.TIMES);
-    assertEquals(expected, actual);		
+		actual = s.nextToken();
+		expected = new Token("*", TokenType.TIMES);
+		assertEquals(expected, actual);		
 
-            actual = s.nextToken();
-            expected = new Token("testing", TokenType.ID);
-    assertEquals(expected, actual);	
+		actual = s.nextToken();
+		expected = new Token("testing", TokenType.ID);
+		assertEquals(expected, actual);	
 
-            /* actual = s.nextToken();
-            expected = new Token("traditional", TokenType.ID);
-    assertEquals(expected, actual);	
+		/* actual = s.nextToken();
+		expected = new Token("traditional", TokenType.ID);
+		assertEquals(expected, actual);	
 
-            actual = s.nextToken();
-            expected = new Token("comment", TokenType.ID);
-    assertEquals(expected, actual);	
+		actual = s.nextToken();
+		expected = new Token("comment", TokenType.ID);
+		assertEquals(expected, actual);	
 
-            actual = s.nextToken();
-            expected = new Token("!", TokenType.NOT);
-    assertEquals(expected, actual);	 */
+		actual = s.nextToken();
+		expected = new Token("!", TokenType.NOT);
+		assertEquals(expected, actual);	 */
 
-            actual = s.nextToken();
+		actual = s.nextToken();
         assertEquals(actual, s.getEND());     
     }
 
@@ -176,7 +176,7 @@ public class ScannerTest {
 	
     /***** SAD TESTS
      * @throws java.lang.Exception *****/
-
+	 
     /* Tests what happens when an illegal symbol is found */
     @Test
     public void testNextTokenSad1() throws Exception {
