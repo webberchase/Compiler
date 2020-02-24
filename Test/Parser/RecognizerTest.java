2b87bc2f3ff50f4d68853198a40ee82ccc7fa4e1
webberc <webberc@augsburg.edu>
2020-02-23 22:00:44 -0600

    added program tests

--- Test/Parser/RecognizerTest.java
+++ Test/Parser/RecognizerTest.java
@@ -878,6 +878,33 @@ public class RecognizerTest {
         assertEquals(rec.getLookahead(), rec.getEND());
     }
 
+/* Happy Test 2 for statementList function */
+    @Test
+    public void statementListHappy2() throws Exception {
+        String test = 
+				"john = -5/5+3 <= 1+1;\n" +
+				"id[1+1] = 5/5+3<=2;\n" +
+				"\n" +
+				"if -5/5+3 <= 1+1 \n" +
+				"	then ident[-5/5+3<=1+1] = -5/5+3<=1+1\n" +
+				"	else ident[-5/5+3<=1+1] = -5/5+3<=1+1;\n" +
+				"\n" +
+				"while -5/5+3 <= 1+1 do \n" +
+				"	ident[-5/5+3<=1+1]=-5/5+3<=1+1;\n" +
+				"\n" +
+				"read(id);\n" +
+				"write(-5/5+3 <= 1+1);\n" +
+				"\n" +
+				"return -5/5+3 <= 1+1\n";
+        Recognizer rec = new Recognizer(test, false);
+        
+        rec.statementList();
+        rec.isEnd();
+        
+        assertEquals(rec.getLookahead(), rec.getEND());
+    }
+
+
     /* Sad Test 1 for statementList function */
     @Test
     public void statementListSad1() throws Exception {
@@ -894,6 +921,35 @@ public class RecognizerTest {
         }
     }
     
+	/* Sad Test 2 for statementList function */
+    @Test
+    public void statementListSad2() throws Exception {
+        String test = 
+				"john = -5/5+3 <= 1+1;\n" +
+				"id[1+1] = 5/5+3<=2;\n" +
+				"\n" +
+				"if -5/5+3 <= 1+1 \n" +
+				"	then ident[-5/5+3<=1+1] = -5/5+3<=1+1\n" +
+				"	else ident[-5/5+3<=1+1] = -5/5+3<=1+1;\n" +
+				"\n" +
+				"while -5/5+3 <= 1+1 do \n" +
+				"	ident[-5/5+3<=1+1]=-5/5+3<=1+1;\n" +
+				"\n" +
+				"read(id);\n" +
+				"write(-5/5+3 <= 1+1);\n" +
+				"\n" +
+				"return -5/5+3 <= 1+1\n;";       
+		Recognizer rec = new Recognizer(test, false);
+
+        try {
+            rec.statementList();
+            rec.isEnd();
+            fail("Yikes! The Sad Test didn't fail!!");
+        } catch (Exception e) {
+            String expected = "Statement Error!";
+            assertEquals(expected, e.getMessage());
+        }
+    }
 	
 	/***** optionalStatements()
      * @throws java.lang.Exception *****/
@@ -1278,4 +1334,246 @@ public class RecognizerTest {
     }
     
     
+    /***** program()
+	 * NOTE: Recognizer's file reader doesn't function yet... 
+	 * @throws java.lang.Exception *****/
+	
+    /* Happy Test 1 for program function */
+    @Test
+    public void programHappy1() throws Exception {
+        String test = "void john (void ident, int ident2, float ident3);\n" +
+"int rachael (void ident); \n" +
+"float bruce (int ident2, float ident3);\n" +
+"\n" +
+"main() {\n" +
+"\n" +
+"	int id1, id2, id3; \n" +
+"	float id4;\n" +
+"	\n" +
+"	john = -5/5+3 <= 1+1;\n" +
+"	id[1+1] = 5/5+3<=2;\n" +
+"	\n" +
+"	{\n" +
+"		int id1, id2, id3; \n" +
+"		float id4;\n" +
+"		\n" +
+"		john = -5/5+3 <= 1+1;\n" +
+"		id[1+1] = 5/5+3<=2;\n" +
+"				\n" +
+"		if -5/5+3 <= 1+1 \n" +
+"			then ident[-5/5+3<=1+1] = -5/5+3<=1+1\n" +
+"			else ident[-5/5+3<=1+1] = -5/5+3<=1+1;\n" +
+"			\n" +
+"		while -5/5+3 <= 1+1 do \n" +
+"			ident[-5/5+3<=1+1]=-5/5+3<=1+1;\n" +
+"			\n" +
+"		read(id);\n" +
+"		write(-5/5+3 <= 1+1);\n" +
+"		\n" +
+"		return -5/5+3 <= 1+1\n" +
+"	};	\n" +
+"	\n" +
+"	if -5/5+3 <= 1+1 \n" +
+"		then ident[-5/5+3<=1+1] = -5/5+3<=1+1\n" +
+"		else ident[-5/5+3<=1+1] = -5/5+3<=1+1;\n" +
+"		\n" +
+"	while -5/5+3 <= 1+1 do \n" +
+"		ident[-5/5+3<=1+1]=-5/5+3<=1+1;\n" +
+"		\n" +
+"	read(id);\n" +
+"	write(-5/5+3 <= 1+1);\n" +
+"	\n" +
+"	return -5/5+3 <= 1+1\n" +
+"}\n" +
+"\n" +
+"void john (void ident, int ident2, float ident3) {\n" +
+"	int id1, id2, id3; \n" +
+"	float id4;\n" +
+"	\n" +
+"	john = -5/5+3 <= 1+1;\n" +
+"	id[1+1] = 5/5+3<=2;\n" +
+"		\n" +
+"	if -5/5+3 <= 1+1 \n" +
+"		then ident[-5/5+3<=1+1] = -5/5+3<=1+1\n" +
+"		else ident[-5/5+3<=1+1] = -5/5+3<=1+1;\n" +
+"		\n" +
+"	while -5/5+3 <= 1+1 do \n" +
+"		ident[-5/5+3<=1+1]=-5/5+3<=1+1;\n" +
+"		\n" +
+"	read(id);\n" +
+"	write(-5/5+3 <= 1+1);\n" +
+"	\n" +
+"	return -5/5+3 <= 1+1\n" +
+"}	\n" +
+"	\n" +
+"int rachael (void ident) {\n" +
+"	int id1, id2, id3; \n" +
+"	float id4;\n" +
+"	\n" +
+"	john = -5/5+3 <= 1+1;\n" +
+"	id[1+1] = 5/5+3<=2;\n" +
+"		\n" +
+"	if -5/5+3 <= 1+1 \n" +
+"		then ident[-5/5+3<=1+1] = -5/5+3<=1+1\n" +
+"		else ident[-5/5+3<=1+1] = -5/5+3<=1+1;\n" +
+"		\n" +
+"	while -5/5+3 <= 1+1 do \n" +
+"		ident[-5/5+3<=1+1]=-5/5+3<=1+1;\n" +
+"		\n" +
+"	read(id);\n" +
+"	write(-5/5+3 <= 1+1);\n" +
+"	\n" +
+"	return -5/5+3 <= 1+1\n" +
+"}	\n" +
+"	\n" +
+"float bruce (int ident2, float ident3) {\n" +
+"	int id1, id2, id3; \n" +
+"	float id4;\n" +
+"	\n" +
+"	john = -5/5+3 <= 1+1;\n" +
+"	id[1+1] = 5/5+3<=2;\n" +
+"		\n" +
+"	if -5/5+3 <= 1+1 \n" +
+"		then ident[-5/5+3<=1+1] = -5/5+3<=1+1\n" +
+"		else ident[-5/5+3<=1+1] = -5/5+3<=1+1;\n" +
+"		\n" +
+"	while -5/5+3 <= 1+1 do \n" +
+"		ident[-5/5+3<=1+1]=-5/5+3<=1+1;\n" +
+"		\n" +
+"	read(id);\n" +
+"	write(-5/5+3 <= 1+1);\n" +
+"	\n" +
+"	return -5/5+3 <= 1+1\n" +
+"}	\n" +
+"";
+        Recognizer rec = new Recognizer(test, false);
+        
+        rec.program();
+        rec.isEnd();
+        
+        assertEquals(rec.getLookahead(), rec.getEND());
+    }
+	
+    /* Sad Test 1 for program function */
+    @Test
+    public void programSad1() throws Exception {
+        String test = "void john (void ident, int ident2, float ident3);\n" +
+"int rachael (void ident); \n" +
+"float bruce (int ident2, float ident3);\n" +
+"\n" +
+"main() {\n" +
+"\n" +
+"	int id1, id2, id3; \n" +
+"	float id4;\n" +
+"	\n" +
+"	john = -5/5+3 <= 1+1;\n" +
+"	id[1+1] = 5/5+3<=2;\n" +
+"	\n" +
+"	{\n" +
+"		int id1, id2, id3; \n" +
+"		float id4;\n" +
+"		\n" +
+"		john = -5/5+3 <= 1+1;\n" +
+"		id[1+1] = 5/5+3<=2;\n" +
+"				\n" +
+"		if -5/5+3 <= 1+1 \n" +
+"			then ident[-5/5+3<=1+1] = -5/5+3<=1+1\n" +
+"			else ident[-5/5+3<=1+1] = -5/5+3<=1+1;\n" +
+"			\n" +
+"		while -5/5+3 <= 1+1 do \n" +
+"			ident[-5/5+3<=1+1]=-5/5+3<=1+1;\n" +
+"			\n" +
+"		read(id);\n" +
+"		write(-5/5+3 <= 1+1);\n" +
+"		\n" +
+"		return -5/5+3 <= 1+1\n" +
+"	};	\n" +
+"	\n" +
+"	if -5/5+3 <= 1+1 \n" +
+"		then ident[-5/5+3<=1+1] = -5/5+3<=1+1\n" +
+"		else ident[-5/5+3<=1+1] = -5/5+3<=1+1;\n" +
+"		\n" +
+"	while -5/5+3 <= 1+1 do \n" +
+"		ident[-5/5+3<=1+1]=-5/5+3<=1+1;\n" +
+"		\n" +
+"	read(id);\n" +
+"	write(-5/5+3 <= 1+1);\n" +
+"	\n" +
+"	return -5/5+3 <= 1+1\n" +
+"}\n" +
+"\n" +
+"void john (void ident, int ident2, float ident3) {\n" +
+"	int id1, id2, id3; \n" +
+"	float id4;\n" +
+"	\n" +
+"	john = -5/5+3 <= 1+1;\n" +
+"	id[1+1] = 5/5+3<=2;\n" +
+"		\n" +
+"	if -5/5+3 <= 1+1 \n" +
+"		then ident[-5/5+3<=1+1] = -5/5+3<=1+1\n" +
+"		else ident[-5/5+3<=1+1] = -5/5+3<=1+1;\n" +
+"		\n" +
+"	while -5/5+3 <= 1+1 do \n" +
+"		ident[-5/5+3<=1+1]=-5/5+3<=1+1;\n" +
+"		\n" +
+"	read(id);\n" +
+"	write(-5/5+3 <= 1+1);\n" +
+"	\n" +
+"	return -5/5+3 <= 1+1\n" +
+"}	\n" +
+"	\n" +
+"int rachael (void ident) {\n" +
+"	int id1, id2, id3; \n" +
+"	float id4;\n" +
+"	\n" +
+"	john = -5/5+3 <= 1+1;\n" +
+"	id[1+1] = 5/5+3<=2;\n" +
+"		\n" +
+"	if -5/5+3 <= 1+1 \n" +
+"		then ident[-5/5+3<=1+1] = -5/5+3<=1+1\n" +
+"		else ident[-5/5+3<=1+1] = -5/5+3<=1+1;\n" +
+"		\n" +
+"	while -5/5+3 <= 1+1 do \n" +
+"		ident[-5/5+3<=1+1]=-5/5+3<=1+1;\n" +
+"		\n" +
+"	read(id);\n" +
+"	write(-5/5+3 <= 1+1);\n" +
+"	\n" +
+"	return -5/5+3 <= 1+1\n" +
+"}	\n" +
+"	\n" +
+"float bruce (int ident2, float ident3) {\n" +
+"	int id1, id2, id3; \n" +
+"	float id4;\n" +
+"	\n" +
+"	john = -5/5+3 <= 1+1;\n" +
+"	id[1+1] = 5/5+3<=2;\n" +
+"		\n" +
+"	if -5/5+3 <= 1+1 \n" +
+"		then ident[-5/5+3<=1+1] = -5/5+3<=1+1\n" +
+"		else ident[-5/5+3<=1+1] = -5/5+3<=1+1;\n" +
+"		\n" +
+"	while -5/5+3 <= 1+1 do \n" +
+"		ident[-5/5+3<=1+1]=-5/5+3<=1+1;\n" +
+"		\n" +
+"	read(id);\n" +
+"	write(-5/5+3 <= 1+1)\n" +
+"	\n" +
+"	return -5/5+3 <= 1+1\n" +
+"}	\n" +
+"";
+        Recognizer rec = new Recognizer(test, false);
+
+        try {
+            rec.program();
+            rec.isEnd();
+            fail("Yikes! The Sad Test didn't fail!!");
+        } catch (Exception e) {
+            String expected = "Match of RCURLY found RETURN instead Error!";
+            assertEquals(expected, e.getMessage());
+        }
+    }
+
+    
+
 }
