5551967439510173b1ce860cbaac441b1cc0ebd3
webberc <webberc@augsburg.edu>
2020-02-29 06:02:33 -0600

    added var and fun methods, error handling

--- Parser/SymbolTable.java
+++ Parser/SymbolTable.java
@@ -9,10 +9,144 @@ import java.util.HashMap;
  */
 public class SymbolTable {
     
-    public HashMap
+    private HashMap<String, STRow> st = null;
 	
-	private class stRow {
-		
+    /**
+     * Basic Constructor
+     */
+    public SymbolTable() {
+        st = new HashMap<String, STRow>();
+    }
+    
+    /***** FunName Methods *****/
+    
+    /**
+     * Adds a new function name to the table.
+     * @param ID name of the function
+     */
+    public void addFunName(String ID) {
+        // Check for duplicate FunName
+        if (st.containsKey(ID)) {
+            if (st.get(ID).kind == Kind.FUN_NAME) {
+                error("Duplicate FunName");
+                return;
+            }
+        }
+        STRow newRow = new STRow(ID, Kind.FUN_NAME);
+        st.put(ID, newRow);
+
+    }
+    
+    /**
+     * Checks if given ID is the name of a function.
+     * @param ID The name we are testing
+     * @return True if this is a function name, otherwise false
+     */
+    public boolean isFunName(String ID) {
+        // Case: This ID is not in the table!
+        if (!st.containsKey(ID)) {
+            return false;
+        }
+        else if (st.get(ID).kind == Kind.FUN_NAME) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    /***** VarName Methods *****/
+    
+    /**
+     * Adds a new variable name to the table.
+     * @param ID name of the variable
+     */
+    public void addVarName(String ID) {
+        // Check for duplicate VarName
+        if (st.containsKey(ID)) {
+            if (st.get(ID).kind == Kind.VAR_NAME) {
+                error("Duplicate VarName");
+                return;
+            }
+        }
+        STRow newRow = new STRow(ID, Kind.VAR_NAME);
+        st.put(ID, newRow);
+    }
+    
+    /**
+     * Checks if given ID is the name of a variable.
+     * @param ID The name we are testing
+     * @return True if this is a variable name, otherwise false
+     */
+    public boolean isVarName(String ID) {
+        // Case: This ID is not in the table!
+        if (!st.containsKey(ID)) {
+            return false;
+        }
+        else if (st.get(ID).kind == Kind.VAR_NAME) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    
+    
+    /**
+     * Internal Structure: 
+     * This is one row of the table!
+     * Variables are equivalent to fields
+     */
+    private class STRow {
+        String ID;
+        Kind kind;  
+        Type type;
+        
+        /** Constructor with unspecified Type */
+        private STRow(String newID, Kind newKind) {
+            this.ID = newID;
+            this.kind = newKind;
+            this.type = null;
+        }            
+
+        /* Constructor with specified Type */
+        private STRow(String newID, Kind newKind, Type newType) {
+            this.ID = newID;
+            this.kind = newKind;
+            this.type = newType;
+        }
+
+        
+        
+    }
+    
+    /**
+     * What kind of IDs are possible?
+     */
+    private enum Kind {
+        FUN_NAME,
+        VAR_NAME,
+    }
+ 
+    /**
+     * What is the type associated with this ID?
+     * If it is a variable, this is the data type
+     * If it is a function, this is the return type
+     */
+    private enum Type {
+        VOID,
+        INT,
+        FLOAT,
+    }
+    
+    /**
+	 * Errors out of the symbol Table.Prints an error message 
+         * @param message The error message to print. 
+	 */
+	public void error(String message) {
+		System.out.println("Symbol Table Error " + message); 
+                throw new RuntimeException(message + " Error in Symbol Table!");
 	}
     
 }
