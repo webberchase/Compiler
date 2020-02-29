431cf19ff602961dca9a13b11f688cc968dd2e3b
webberc <webberc@augsburg.edu>
2020-02-29 05:30:47 -0600

    created internal structure and enums, constructor

---
+++ Parser/SymbolTable.java
@@ -0,0 +1,18 @@
+package parser;
+
+import java.util.HashMap;
+
+
+/**
+ * 
+ * @author Chase Webber
+ */
+public class SymbolTable {
+    
+    public HashMap
+	
+	private class stRow {
+		
+	}
+    
+}
